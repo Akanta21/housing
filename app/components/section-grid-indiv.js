6d4fcd18b5c7390788180684fd9d93a6eeef85d6
Isabella Ong <isabellaong@gmail.com>
2016-08-10 21:38:22 +0800

    adds update cell function to update home id

--- app/components/section-grid-indiv.js
+++ app/components/section-grid-indiv.js
@@ -13,28 +13,9 @@ export default Ember.Component.extend({
     else return null
   }),
 
-  residents: [],
-
   click: function (event) {
     console.log('CLICK')
     console.log(event.target)
-    if (this.get('cell').occupied) {
-      var occupiedCellId = this.get('cell').id
-      var filterCell = this.get('cells').filter(cell => { return parseInt(cell.get('id'), 10) === occupiedCellId })
-      var filterHome = this.get('homes').filter(home => { return parseInt(home.get('id'), 10) === filterCell[0].get('home').id })
-      console.log('HOME ID: ', filterHome[0].get('id'))
-      this.set('residents', filterHome[0].get('users'))
-      console.log(this.get('residents'))
-      var residents = filterHome[0].get('users')
-      this.attrs.save(residents)
-    } else {
-      // Option to add cell to home
-    }
+    this.attrs.save(this.get('cell'))
   }
-
-  // actions: {
-  //   selectCell: function () {
-  //     console.log('select')
-  //   }
-  // }
 })
--- app/controllers/sections/show.js
+++ app/controllers/sections/show.js
@@ -1,12 +1,39 @@
 import Ember from 'ember'
 
 export default Ember.Controller.extend({
+  sessionAccount: Ember.inject.service(),
   residents: [],
+  optionToOccupy: false,
+  optionToVacate: false,
+  emptyCell: '',
 
   actions: {
-    saveResult (newData) {
-      console.log(newData)
-      this.set('residents', newData)
+    checkCell (cell) {
+      if (cell.occupied) {
+        this.set('optionToOccupy', false)
+        console.log('occupied')
+        var occupiedCellId = cell.id
+        var filterCell = this.get('model.cells').filter(cell => { return parseInt(cell.get('id'), 10) === occupiedCellId })
+        var filterHome = this.get('model.homes').filter(home => { return parseInt(home.get('id'), 10) === filterCell[0].get('home').id })
+        this.set('residents', filterHome[0].get('users'))
+        if (parseInt(this.get('sessionAccount').currentUser.get('home').id, 10) === parseInt(filterHome[0].id, 10)) {
+          console.log('match!')
+          this.set('optionToVacate', true)
+        } else {
+          this.set('optionToVacate', false)
+        }
+      } else {
+        console.log('empty')
+        this.set('optionToOccupy', true)
+        var emptyCellId = cell.id
+        var filterEmptyCell = this.get('model.cells').filter(cell => { return parseInt(cell.get('id'), 10) === emptyCellId })
+        this.set('emptyCell', filterEmptyCell[0])
+      }
+    },
+
+    updateCell (cell) {
+      cell.save()
+      window.location.reload(true)
     }
   }
 })
