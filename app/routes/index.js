cb7d9815561a2452d8f4e345eafa32e76343c2a3
Isabella Ong <isabellaong@gmail.com>
2016-08-10 12:02:22 +0800

    adds create home function

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,5 +9,13 @@ export default Ember.Route.extend({
   // }
   model () {
     return this.store.createRecord('home')
+  },
+
+  actions: {
+    createHome () {
+      var newHome = this.get('currentModel')
+      newHome.save()
+      // this.transitionTo('sections')
+    }
   }
 })
