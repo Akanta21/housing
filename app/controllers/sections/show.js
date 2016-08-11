7aae1117e6e0d2b316261452c45912f5c472064b
Isabella Ong <isabellaong@gmail.com>
2016-08-12 01:54:57 +0800

    adds index svg

--- app/controllers/sections/show.js
+++ app/controllers/sections/show.js
@@ -6,6 +6,7 @@ export default Ember.Controller.extend({
   optionToOccupy: false,
   optionToVacate: false,
   emptyCell: '',
+  color: '',
 
   actions: {
     checkCell (cell) {
@@ -16,6 +17,7 @@ export default Ember.Controller.extend({
         var filterCell = this.get('model.cells').filter(cell => { return parseInt(cell.get('id'), 10) === occupiedCellId })
         var filterHome = this.get('model.homes').filter(home => { return parseInt(home.get('id'), 10) === filterCell[0].get('home').id })
         this.set('residents', filterHome[0].get('users'))
+        this.set('color', filterHome[0].get('color'))
         if (parseInt(this.get('sessionAccount').currentUser.get('home').id, 10) === parseInt(filterHome[0].id, 10)) {
           console.log('match!')
           this.set('optionToVacate', true)
@@ -26,6 +28,7 @@ export default Ember.Controller.extend({
         console.log('empty')
         this.set('optionToOccupy', true)
         this.set('optionToVacate', false)
+        this.set('color', 'black')
         var emptyCellId = cell.id
         var filterEmptyCell = this.get('model.cells').filter(cell => { return parseInt(cell.get('id'), 10) === emptyCellId })
         this.set('emptyCell', filterEmptyCell[0])
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,4 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
-  // model () {
-  //   return this.store.createRecord('home')
-  // },
-  //
-  // actions: {
-  //   createHome () {
-  //     var newHome = this.get('currentModel')
-  //     newHome.save()
-  //     this.transitionTo('sections')
-  //   }
-  // }
 })
